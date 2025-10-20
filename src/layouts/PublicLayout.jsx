e9216a5d33d061cc0c4ad4f0947ff33f70aa1d0c
Akash <akash.enlighten@gmail.com>
2025-10-19 22:03:51 -0700

    Added footer

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,5 +1,8 @@
 import { Outlet } from "react-router-dom";
+
 import Header from "../components/Header";
+
+import Footer from "../components/Footer";
 
 export default function PublicLayout() {
   return (
@@ -8,6 +11,7 @@ export default function PublicLayout() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
